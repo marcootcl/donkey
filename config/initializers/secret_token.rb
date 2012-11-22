e99fae0f8143e324d6d980cfc138b289803a1597
Martin Cooper <martin.cooper@trant.co.uk>
2012-11-22 10:27:35 +0000

    Created a app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Donkey::Application.config.secret_token = '6c976092e5bb6fd7feef78d5deecf96154f9552f650e0126e15b8834556ebf229347aa9b096c9fa32341204fbd38e3c0336c00c9f9e9f36590f38ea84dcb233b'
