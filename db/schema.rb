a6a2735a3fbafbc6ecdd0f66b1a164e9704c4cd5
Stu Rice <stuedric@gmail.com>
2012-11-22 11:42:17 +0000

    added stable

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20121122112229) do
+
+  create_table "stables", :force => true do |t|
+    t.integer  "number"
+    t.boolean  "window"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
